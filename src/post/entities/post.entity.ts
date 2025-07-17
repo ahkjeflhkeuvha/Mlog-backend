10d9dad4a66a329db84af57e2980cfe91b68ebfd
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-07-17 09:45:59 +0900

    feat: add Post entity with basic columns

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,31 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  thumbnail_url: string;
+
+  @Column()
+  is_uploaded: boolean;
+
+  @Column()
+  is_deleted: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  // board_id : ManyToOne
+
+  // user_id : ManyToOne
+}
