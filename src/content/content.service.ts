3175814cdad85ac5aa25931d3124715456d13fae
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-07-27 13:49:09 +0900

    feat: add transaction interceptor in comment

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -3,7 +3,7 @@ import { CreateContentDto } from './dto/create-content.dto';
 import { UpdateContentDto } from './dto/update-content.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Content } from './entities/content.entity';
-import { Repository } from 'typeorm';
+import { QueryRunner, Repository } from 'typeorm';
 import { Post } from '../post/entities/post.entity';
 
 @Injectable()
@@ -15,8 +15,8 @@ export class ContentService {
     private readonly postRepository: Repository<Post>,
   ) {}
 
-  async create(createContentDto: CreateContentDto) {
-    const post = await this.postRepository.findOne({
+  async create(createContentDto: CreateContentDto, qr: QueryRunner) {
+    const post = await qr.manager.findOne(Post, {
       where: {
         id: createContentDto.post_id,
       },
@@ -26,19 +26,19 @@ export class ContentService {
       throw new NotFoundException('포스트가 존재하지 않습니다.');
     }
 
-    const content = this.contentRepository.create({ content: createContentDto.content, post });
-    await this.contentRepository.save(content);
+    const content = qr.manager.create(Content, { content: createContentDto.content, post });
+    await qr.manager.save(Content, content);
 
     return content.id;
   }
 
-  async update(content_id: number, updateContentDto: UpdateContentDto) {
-    await this.contentRepository.update({ id: content_id }, { ...updateContentDto });
+  async update(content_id: number, updateContentDto: UpdateContentDto, qr: QueryRunner) {
+    await qr.manager.update(Content, { id: content_id }, { ...updateContentDto });
     return content_id;
   }
 
-  async remove(content_id: number) {
-    await this.contentRepository.delete(content_id);
+  async remove(content_id: number, qr: QueryRunner) {
+    await qr.manager.delete(Content, content_id);
     return content_id;
   }
 }
